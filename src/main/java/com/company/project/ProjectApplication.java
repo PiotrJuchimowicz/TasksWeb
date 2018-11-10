eefab9aabf785148bce5e7a5082451317ebb897d
Piotr Juchimowicz <piotrjuchimowicz@gmail.com>
2018-11-10 17:58:48 +0100

    removed unnecesary code from ProjectApplication.java

--- src/main/java/com/company/project/ProjectApplication.java
+++ src/main/java/com/company/project/ProjectApplication.java
@@ -1,5 +1,7 @@
 package com.company.project;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
